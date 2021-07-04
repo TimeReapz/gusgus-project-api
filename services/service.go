754a57324ae053792feec9e1bd634b3014f87877
TimeReapz <souldarkfear@gmail.com>
2021-07-04 16:50:03 +0700

    proxy

--- services/service.go
+++ services/service.go
@@ -8,6 +8,7 @@ import (
 func WriteResponse(w http.ResponseWriter, status int, res interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(res)
 }
